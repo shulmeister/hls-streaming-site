0bf7b8956af6e3796f7502a6bb5477c59ee1be4a
shulmeister <jason@coloradocareassist.com>
2025-07-28 01:26:44 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Start ffmpeg
+echo "Starting HLS streaming server..."
+
+# Make sure we have the required directory
+mkdir -p ./tmp/hls
+
+# Start ffmpeg in background
+echo "Starting FFmpeg..."
 bash ffmpeg.sh
 
+# Wait a moment for ffmpeg to start
+sleep 2
+
 # Start the server
+echo "Starting web server on http://localhost:3001"
 npm start
