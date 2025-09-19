09d4f7db2a9ac136318432f5a6c18f58889369bf
shulmeister <jason@coloradocareassist.com>
2025-09-19 13:53:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 const PORT = 3001;
 
-// Serve the index.html
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve HLS stream
-app.use('/hls', express.static(path.join(__dirname, 'tmp/hls')));
+app.use('/stream', express.static('stream'));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 HLS stream server running at http://143.198.144.51:${PORT}/stream/index.m3u8`);
 });
